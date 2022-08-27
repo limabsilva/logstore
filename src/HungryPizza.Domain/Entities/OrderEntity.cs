ec675f23ee6286eb2d6f5f10b2b13df2c4110e96
Bruno Lima <limabsilva@outlook.com>
2022-08-27 08:05:34 -0300

    Models Created

---
+++ src/HungryPizza.Domain/Entities/OrderEntity.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HungryPizza.Domain.Entities;
+public class OrderEntity : BaseEntity
+{
+    public int ClientID { get; set; }
+    public ClientEntity? Client { get; set; }
+    public string? Description { get; set; }
+    public decimal PriceTotal { get; set; }
+    public ICollection<OrderItemEntity>? OrderItems { get; set; }
+
+}
